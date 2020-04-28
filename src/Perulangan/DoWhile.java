15446541ac15627b1799b441e0990486253a7272
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-28 15:23:21 +0800

    Perulangan DoWhile

--- src/Perulangan/DoWhile.java
+++ src/Perulangan/DoWhile.java
@@ -2,16 +2,16 @@ package Perulangan;
 
 public class DoWhile {
     public static void main(String[] args) {
-        int isiGelas = 225;
+        int isiGelas = 226;
         int penuh    = 225;
 
-        //Apa yang terjadi jika sedari awal nilai int isiGelas = 225; ?
-        //Setelah code dijalankan, baru dicek apakah kondisinya memenuhi.
-        //Yang Terjadi : Tidak ada perulangan karena kondisi memenuhi syarat. Tetapi, code isiGelas++ tetap dijalankan.
-        // isi gelas awalnya 225, kemudian di increment, lalu dicek (apakah isiGelas <= penuh?).
-        // Ternyata iya. Maka, proses perulangan terhenti dan nilai akhir isiGelas jdi 226 karena sdh di increment.
-
-
+        // Apa yang terjadi jika sedari awal nilai int isiGelas = 225; ?
+        // Akan dilakukan perulangan, karena kondisinya bernilai true.
+        // Walapun kondisinya false, isiGelas akan tetap ditambah.
+        // Karena DoWhile pd saat compile, dia akan eksekusi kode baru di cek kondisinya.
+        // berhubung ada perintah increment, jd hasil akhir bakal ada ketambahan nilai.
+        // value penuh = 225 menjadi batas nilai yg akan di increment, jika value isiGelas > value penuh
+        // Maka hasilnya akan looping sebanyak ~
 
         System.out.println("Isi Gelas Sekarang      : "+isiGelas+"ml");
         System.out.println("Isi Gelas Ketika Penuh  : "+penuh+"ml");
@@ -20,7 +20,7 @@ public class DoWhile {
             isiGelas++;
             System.out.println("Sedang mengisi gelas .... ");
             System.out.println("Isi Gelas Sekarang  : "+isiGelas+"ml");
-        } while (isiGelas != penuh);
+        } while (isiGelas <= penuh);
 
         System.out.println("");
         System.out.println("Final : Isi Gelas Sekarang : "+isiGelas+"ml");
