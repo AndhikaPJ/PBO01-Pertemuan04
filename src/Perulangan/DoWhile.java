28056bf9c2937146bab7423b98e81890e5c1637b
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-28 14:33:39 +0800

    Perulangan DoWhile

---
+++ src/Perulangan/DoWhile.java
@@ -0,0 +1,28 @@
+package Perulangan;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int isiGelas = 225;
+        int penuh    = 225;
+
+        //Apa yang terjadi jika sedari awal nilai int isiGelas = 225; ?
+        //Setelah code dijalankan, baru dicek apakah kondisinya memenuhi.
+        //Yang Terjadi : Tidak ada perulangan karena kondisi memenuhi syarat.
+        // isi gelas awalnya 225, kemudian di increment, lalu dicek (apakah isiGelas <= penuh?).
+        // Ternyata iya. Maka, proses perulangan terhenti dan nilai akhir isiGelas jdi 226 karena sdh di increment.
+
+
+
+        System.out.println("Isi Gelas Sekarang      : "+isiGelas+"ml");
+        System.out.println("Isi Gelas Ketika Penuh  : "+penuh+"ml");
+
+        do {
+            isiGelas++;
+            System.out.println("Sedang mengisi gelas .... ");
+            System.out.println("Isi Gelas Sekarang  : "+isiGelas+"ml");
+        } while (isiGelas <= penuh);
+
+        System.out.println("");
+        System.out.println("Final : Isi Gelas Sekarang : "+isiGelas+"ml");
+    }
+}
--- src/Perulangan/Main.java
+++ src/Perulangan/Main.java
@@ -1,4 +1,4 @@
-package Perulangan;
+package Perulangan; //Contoh Perulangan While
 
 public class Main {
     public static void main(String[] args) {
